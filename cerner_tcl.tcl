9609bed3d16265b8a45ecf108c34d3037898051c
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2020-09-15 15:34:25 +0530

    cerner_tcl
    2nd program

---
+++ cerner_tcl.tcl
@@ -0,0 +1,8 @@
+#cerner_2^5_2020
+#This obfuscated code prints Cerner
+interp alias {} || {} set ; || ___ -84; || __ -1079
+|| c 1645;|| :_ -1287;|| _. 515;interp alias {} [] \
+{} incr;  || _ 290 ;  || _c {};binary scan Coding c*\
+chars;foreach .. $chars {append _c [format %c [[] .. \
+[[] _ [[] __ [[] c [ [] :_ [ [] _. $___] ] ] ] ] ] ] }; 
+puts $_c
