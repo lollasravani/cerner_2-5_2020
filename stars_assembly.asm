669aec3021e678e3db296d9f846a622a7c0fe541
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2020-09-17 19:37:05 +0530

    stars_assembly
    4th program

---
+++ stars_assembly.asm
@@ -0,0 +1,21 @@
+; cerner_2^5_2020
+section .text
+ global main
+ %define Happy mov
+ %define Programmers mov
+ %define Day mov
+ %define to mov
+ %define all int
+ %define participants mov
+ %define Enjoy int
+ %define Coding section
+main:
+Happy edx,9
+Programmers ecx, AllTheBest
+Day ebx,1
+to eax,4
+all 0x80
+participants eax,1
+Enjoy 0x80
+Coding .data
+AllTheBest times 32 db '*'
