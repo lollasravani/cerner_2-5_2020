d993dc5eee4d087e8ef2da71667e9537073373e8
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2020-09-20 19:02:50 +0530

    super_dart
    7th program

---
+++ super_dart.dart
@@ -0,0 +1,16 @@
+//cerner_2^5
+//This program shows the use of super
+void main() { 
+   Child child = new Child(); 
+   child.method(32); 
+} 
+class Parent { 
+   void method(string parentVariable){ print("value of parentVariable ${parentVariable}");} 
+} 
+class Child extends Parent { 
+   @override 
+   void method(int childVariable) { 
+      print("value of childVariable ${childVariable}"); 
+      super.method("2^5"); 
+   } 
+}
