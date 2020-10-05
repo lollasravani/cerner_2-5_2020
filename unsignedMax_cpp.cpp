aea91ec39f0c3b2086a75f0bb6bffb0e86fd512e
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2020-10-05 18:54:42 +0530

    22nd program

---
+++ unsignedMax_cpp.cpp
@@ -0,0 +1,24 @@
+// cerner_2^5_2020
+//The below program will print the maximum value of an unsigned int
+#include <iostream>;
+#define obfusCate using
+#define obFuscate namespace
+#define obfUscate std
+#define obFUsCate int
+#define OBfuscaTE main
+#define oBfuscAtE {
+#define oBfuScatE }
+#define OBFUScate unsigned
+#define OBFuscATE 0
+#define obfusCatE ~
+#define oBfUscaTe =
+#define oBfUScate "%u",
+#define OBFUSCATE printf
+#define obFuScAte return
+#define obfuscaTe ;
+#define OBFUSCAte (
+#define obfusCATE ) 
+
+obfusCate obFuscate obfUscate;
+
+obFUsCate OBfuscaTE OBFUSCAte obfusCATE oBfuscAtE OBFUScate obFUsCate obfuscate obfuscaTe obfuscate oBfUscaTe OBFuscATE obfuscaTe obfuscate oBfUscaTe obfusCatE OBFUSCAte obfuscate obfusCATE obfuscaTe OBFUSCATE OBFUSCAte oBfUScate  obfuscate obfusCATE obfuscaTe obFuScAte OBFuscATE obfuscaTe oBfuScatE
