f1034522ac3d053769ea84906d3feb3d2162168c
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2020-10-06 21:15:57 +0530

    alphaTriangle.cs

---
+++ alphaTriangle.cs
@@ -0,0 +1,23 @@
+// cerner_2^5_2020
+// This program prints alphabet triangle
+using System;  
+  public class PrintExample  
+   {  
+     public static void Main(string[] args)  
+      {  
+       char ch='A';      
+       int i, j, k, m;      
+       for(i=1; i<=5; i++)      
+       {      
+        for(j=5; j>=i; j--)      
+         Console.Write(" ");      
+        for(k=1;k<=i;k++)      
+         Console.Write(ch++);      
+        ch--;      
+        for(m=1;m<i;m++)      
+         Console.Write(--ch);      
+        Console.Write("\n");      
+        ch='A';      
+       }      
+   }  
+  }  
