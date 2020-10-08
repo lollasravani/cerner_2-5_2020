0f51de8570454ab60e203f8df2e047806376f628
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2020-10-08 18:46:29 +0530

    25th program

---
+++ ignoreInput_c.c
@@ -0,0 +1,12 @@
+// cerner_2^5_2020
+// We can use '*' in scanf to ignore an input
+#include<stdio.h> 
+int main() 
+{ 
+    int a; 
+    scanf("%*d%d", &a); 
+  
+    printf( "%d ",  a);   
+  
+    return 0;      
+} 
