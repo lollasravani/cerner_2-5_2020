cfc0bc7d2a37a3a73f6974e857f2435d83a03047
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2020-10-14 16:07:15 +0530

    nullObject_php

---
+++ nullObject_php.php
@@ -0,0 +1,9 @@
+<?php
+// cernre_2^5_2020
+// When a variable is declared as a null object...Then it is not empty 
+$a = null;
+echo empty($a) ? "Empty\n" : "Not Empty\n";
+
+$a = (object) null;
+echo empty($a) ? "Empty" : "Not Enpty";
+?>
